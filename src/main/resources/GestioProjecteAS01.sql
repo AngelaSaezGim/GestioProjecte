407830aad1ee28281e4ca23af7bc3acf704154f0
AngelaSaezGim <angelasaezgim12@gmail.com>
2025-01-09 08:23:28 +0100

    SQL completado script

--- src/main/resources/GestioProjecteAS01.sql
+++ src/main/resources/GestioProjecteAS01.sql
@@ -27,9 +27,11 @@ CREATE TABLE IF NOT EXISTS TascaAS01 (
 	FOREIGN KEY (idProjecte) REFERENCES ProjecteAS01(idProjecte)
 );
 
-/*4. OperariResponsable: IdOperariTasca (PK, AI), NIFOperari (NN), IdTasca (NN), observacions*/
+/*4. OperariResponsable: IdOperariTasca (PK, AI), nom, cognom, NIFOperari (NN), IdTasca (NN), observacions*/
 CREATE TABLE IF NOT EXISTS OperariResponsableAS01 (
     idOperariTasca INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(45),
+    cognom VARCHAR(45),
     nifOperari VARCHAR(9) NOT NULL,
     idTasca INT NOT NULL,
     observacions VARCHAR(255),
@@ -45,3 +47,45 @@ CREATE TABLE IF NOT EXISTS FacturaAS01 (
     FOREIGN KEY (idTasca) REFERENCES TascaAS01(idTasca),
     FOREIGN KEY (idClient) REFERENCES ClientAS01(idClient)
 );
+
+-- Com a mínim caldrà afegir les següents dades (completar en cas de camps claus o no nuls)
+
+-- CLIENT: Registrat a tu mateixa com a client
+
+INSERT INTO ClientAS01 (nom, cognom, nif)
+VALUES ('Ángela', 'Sáez', '73665652X');
+
+-- PROJECTE: Registra 4 projectes que portaran com a nom “Construcció de la casa de XXXXXXXX “ on XXXXXXX seran 4 companys de classe.
+INSERT INTO ProjecteAS01 (descripcio, estat, idClient)
+VALUES 
+    ('Construcció de la casa de Raquel', 'No iniciat', 1),
+    ('Construcció de la casa de Marcos', 'En procés', 1),
+    ('Construcció de la casa de Ruben', 'Finalitzat', 1),
+    ('Construcció de la casa de David', 'No iniciat', 1);
+
+-- TASCA: Inventa 2 tasques per 2 dels projectes anteriors i els altres 2 els deixes sense tasques. Finalitza 1 tasca per cadascun del 2 projectes amb tasques.
+INSERT INTO TascaAS01 (descripcio, estat, idProjecte)
+VALUES 
+    ('Preparar el terreny', 'Finalitzat', 2),
+    ('Construir els fonaments', 'En procés', 2),
+    ('Dissenyar plànols', 'Finalitzat', 3),
+    ('Organitzar subministraments', 'En procés', 3);
+
+-- OPERARI RESPONSABLE: Registra a Pepe Goteras com a responsable de les dues tasques. NIF 12345678A.
+INSERT INTO OperariResponsableAS01 (nifOperari, nom, cognom, idTasca, observacions)
+VALUES 
+    ('12345678A', 'Pepe', 'Goteras', 1, 'Tasques inicials completades per Pepe Goteras'),
+    ('12345678A', 'Pepe', 'Goteras', 3, 'Tasques de planificació gestionades per Pepe Goteras');
+
+
+-- FACTURA:Emet una factura amb data 31/12/2017 i una altra amb data 30/11/2024.
+INSERT INTO FacturaAS01 (idTasca, idClient, data)
+VALUES 
+    (1, 1, '2017-12-31'),
+    (3, 1, '2024-11-30');
+    
+SELECT * FROM ClientAS01;
+SELECT * FROM ProjecteAS01;
+SELECT * FROM TascaAS01;
+SELECT * FROM OperariResponsableAS01;
+SELECT * FROM FacturaAS01;
